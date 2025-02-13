3974410998216d23dd1a06b7655ad44f2ccfe242
Keisuke Ota <k.ota109@outlook.com>
2025-02-13 23:56:38 +0900

    populate_by_nameを試す

--- adapter/transfer/trivia_transfer.py
+++ adapter/transfer/trivia_transfer.py
@@ -1,11 +1,14 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import List, Optional
 
 class Item(BaseModel):
-    quiz: str
-    choices: List[str]
-    answer: int
-    explanation: str
+    quiz: str = Field
+    choices: List[str] = Field
+    answer: int = Field
+    explanation: str = Field
+
+    # class Config:
+    #     populate_by_name = True
 
 class TriviaGenerateRequest(BaseModel):
     category: Optional[str]
